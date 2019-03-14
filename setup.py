ad10d55c67bb37e8845682084606b70bdcf67609
Victor Fernandez de Alba <sneridagh@gmail.com>
2019-03-14 12:40:41 +0100

    p.restapi as dep

--- setup.py
+++ setup.py
@@ -42,9 +42,7 @@ setup(
         'Products.GenericSetup>=1.8.2',
         'setuptools',
         'z3c.jbot',
-        # 'plone.restapi',
-        # 'Products.PloneFormGen',
-        # 'plone.formwidget.captcha',
+        'plone.restapi',
     ],
     extras_require={
         'test': [

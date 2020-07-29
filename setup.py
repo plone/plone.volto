5e604ee5f2cf5adeb9231f0edc48cc6d81979f75
Timo Stollenwerk <stollenwerk@kitconcept.com>
2020-07-29 16:14:28 +0200

    Back to development: 1.4.1
    [ci skip]

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ long_description = "\n\n".join(
 
 setup(
     name="kitconcept.volto",
-    version="1.4.0",
+    version="1.4.1.dev0",
     description="Volto integration add-on for Plone",
     long_description=long_description,
     # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers

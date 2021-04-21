68788d0319bc5c39b4dc38793cd38d8e6605aab3
Victor Fernandez de Alba <sneridagh@gmail.com>
2021-04-21 18:28:36 +0200

    Back to development: 2.2.2

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ long_description = "\n\n".join(
 
 setup(
     name="kitconcept.volto",
-    version="2.2.1",
+    version="2.2.2.dev0",
     description="Volto integration add-on for Plone",
     long_description=long_description,
     # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers

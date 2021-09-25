fac4a554e8e4c3b400ec14682cd47ee74e8cd723
Timo Stollenwerk <stollenwerk@kitconcept.com>
2021-09-25 11:39:31 +0200

    Back to development: 3.0.0a2

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ long_description = "\n\n".join(
 
 setup(
     name="plone.volto",
-    version="3.0.0a1",
+    version="3.0.0a2.dev0",
     description="Volto integration add-on for Plone",
     long_description=long_description,
     # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers

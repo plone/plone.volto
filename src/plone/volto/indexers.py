3b6c750686fcbefcfd92074e0725be5e0fc01a59
Victor Fernandez de Alba <sneridagh@gmail.com>
2021-10-11 12:18:27 +0200

    WIP

--- src/plone/volto/indexers.py
+++ src/plone/volto/indexers.py
@@ -1,5 +1,6 @@
 from plone.volto.behaviors.preview import IPreview
 from plone.indexer.decorator import indexer
+from plone.dexterity.interfaces import IDexterityContent
 
 
 @indexer(IPreview)
@@ -11,3 +12,15 @@ def hasPreviewImage(obj):
     if obj.aq_base.preview_image:
         return True
     return False
+
+
+@indexer(IDexterityContent)
+def image_field_indexer(obj):
+    """Indexer for knowing in a catalog search if a content has any image."""
+    base_obj = aq_base(obj)
+    image_field = ""
+    if getattr(base_obj, "preview_image"):
+        image_field = "preview_image"
+    elif getattr(base_obj, "image"):
+        image_field = "image"
+    return image_field

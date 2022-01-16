16261224e931c81dcc1e1048ef651041dafcd126
Victor Fernandez de Alba <sneridagh@gmail.com>
2022-01-16 15:23:43 +0100

    Organize the data for the default roots on its own folder

---
+++ src/plone/volto/default_homepage/lrf.py
@@ -0,0 +1,55 @@
+default_lrf_home = {
+    "draftJS": {
+        "blocks": {
+            "15068807-cfc9-444a-97db-8c736809ff52": {"@type": "title"},
+            "59d41d8a-ef05-4e21-8820-2a64f5878092": {
+                "@type": "text",
+                "text": {
+                    "blocks": [
+                        {
+                            "key": "618bl",
+                            "text": "Nulla porttitor accumsan tincidunt. Sed porttitor lectus nibh. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Nulla porttitor accumsan tincidunt. Nulla porttitor accumsan tincidunt. Nulla porttitor accumsan tincidunt. Quisque velit nisi, pretium ut lacinia in, elementum id enim. Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui. Sed porttitor lectus nibh. Pellentesque in ipsum id orci porta dapibus.",
+                            "type": "unstyled",
+                            "depth": 0,
+                            "inlineStyleRanges": [],
+                            "entityRanges": [],
+                            "data": {},
+                        }
+                    ],
+                    "entityMap": {},
+                },
+            },
+        },
+        "blocks_layout": {
+            "items": [
+                "15068807-cfc9-444a-97db-8c736809ff52",
+                "59d41d8a-ef05-4e21-8820-2a64f5878092",
+            ]
+        },
+    },
+    "slate": {
+        "blocks": {
+            "15068807-cfc9-444a-97db-8c736809ff52": {"@type": "title"},
+            "59d41d8a-ef05-4e21-8820-2a64f5878092": {
+                "@type": "slate",
+                "value": [
+                    {
+                        "type": "p",
+                        "children": [
+                            {
+                                "text": "Nulla porttitor accumsan tincidunt. Sed porttitor lectus nibh. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Nulla porttitor accumsan tincidunt. Nulla porttitor accumsan tincidunt. Nulla porttitor accumsan tincidunt. Quisque velit nisi, pretium ut lacinia in, elementum id enim. Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui. Sed porttitor lectus nibh. Pellentesque in ipsum id orci porta dapibus."
+                            }
+                        ],
+                    }
+                ],
+                "plaintext": "Nulla porttitor accumsan tincidunt. Sed porttitor lectus nibh. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Nulla porttitor accumsan tincidunt. Nulla porttitor accumsan tincidunt. Nulla porttitor accumsan tincidunt. Quisque velit nisi, pretium ut lacinia in, elementum id enim. Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui. Sed porttitor lectus nibh. Pellentesque in ipsum id orci porta dapibus.",
+            },
+        },
+        "blocks_layout": {
+            "items": [
+                "15068807-cfc9-444a-97db-8c736809ff52",
+                "59d41d8a-ef05-4e21-8820-2a64f5878092",
+            ]
+        },
+    },
+}

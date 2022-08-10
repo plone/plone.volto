c32f054e935051aaf7302fb43e43b201aa985525
David Glick <david@glicksoftware.com>
2022-08-10 15:17:52 -0700

    Include preview_image_link scales in image_scales catalog metadata

--- src/plone/volto/behaviors/preview_link.py
+++ src/plone/volto/behaviors/preview_link.py
@@ -1,12 +1,20 @@
 # -*- coding: utf-8 -*-
-from plone.volto import _
-from plone.autoform.interfaces import IFormFieldProvider
+from plone import api
+from plone.app.z3cform.widget import RelatedItemsFieldWidget
 from plone.autoform import directives
+from plone.autoform.interfaces import IFormFieldProvider
+from plone.base.interfaces import IImageScalesFieldAdapter
+from plone.rfc822.interfaces import IPrimaryFieldInfo
 from plone.supermodel import model
+from plone.volto import _
+from z3c.form.util import getSpecification
+from z3c.relationfield.schema import RelationChoice
+from zope.component import adapter
+from zope.component import queryMultiAdapter
+from zope.interface import Interface
+from zope.interface import implementer
 from zope.interface import provider
 from zope.schema import TextLine
-from z3c.relationfield.schema import RelationChoice
-from plone.app.z3cform.widget import RelatedItemsFieldWidget
 
 
 @provider(IFormFieldProvider)
@@ -34,3 +42,30 @@ class IPreviewLink(model.Schema):
     preview_caption_link = TextLine(
         title=_("Preview image caption"), description="", required=False
     )
+
+
+@adapter(getSpecification(IPreviewLink["preview_image_link"]), Interface, Interface)
+@implementer(IImageScalesFieldAdapter)
+class PreviewImageScalesFieldAdapter:
+    """Get the image_scales for the preview_image_link field"""
+
+    def __init__(self, field, context, request):
+        self.field = field
+        self.context = context
+        self.request = request
+
+    def __call__(self):
+        value = self.field.get(self.context)
+        linked_image = value.to_object
+        primary_field = IPrimaryFieldInfo(linked_image).field
+        serializer = queryMultiAdapter(
+            (primary_field, linked_image, self.request), IImageScalesFieldAdapter
+        )
+        if serializer is not None:
+            values = serializer()
+            if values:
+                portal_url = api.portal.get().absolute_url()
+                base_path = linked_image.absolute_url().replace(portal_url, "")
+                for value in values:
+                    value["base_path"] = base_path
+            return values

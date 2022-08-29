5d5598f2e59baa5df9c9fcd3f8e3d6a631fa7aa7
Andrea Cecchi <andrea.cecchi85@gmail.com>
2022-08-29 16:29:49 +0200

    preserve order during migrations

--- src/plone/volto/upgrades.py
+++ src/plone/volto/upgrades.py
@@ -1,4 +1,5 @@
 from copy import deepcopy
+from OFS.interfaces import IOrderedContainer
 from plone import api
 from plone.restapi.behaviors import IBlocks
 from plone.volto import content
@@ -17,10 +18,10 @@ MIGRATION = {
 def migrate_content_classes(context):
     """Migrate content created with collective.folderishtypes to plone.volto."""
     interface = "collective.folderishtypes.interfaces.IFolderishType"
-    idxs = [
-        "object_provides",
-    ]
-    brains = api.content.find(object_provides=interface)
+    idxs = ["object_provides", "getObjPositionInParent"]
+    brains = api.content.find(
+        object_provides=interface, sort_on="getObjPositionInParent"
+    )
     total_brains = len(brains)
     logger.info(f"Migration: {total_brains} contents to be migrated.")
     for idx, brain in enumerate(brains):
@@ -28,9 +29,16 @@ def migrate_content_classes(context):
         content_id = content.getId()
         content.__class__ = MIGRATION[content.portal_type]
         parent = content.aq_parent
+        ordered = IOrderedContainer(parent, None)
+        if ordered is not None:
+            order = ordered.getObjectPosition(content.getId())
+            if order == 1:
+                # can be the default one and we will lose the ordering
+                order = ordered.keys().index(content.getId())
         parent._delOb(content_id)
         parent._setOb(content_id, content)
         content = parent[content_id]
+        ordered.moveObjectToPosition(content.getId(), order)
         content.reindexObject(idxs=idxs)
 
         if idx and idx % 100 == 0:

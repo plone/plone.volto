482e21b78a55e438c7dc8b0ff50fcb129bb49e6b
David Glick <david@glicksoftware.com>
2024-10-30 12:54:32 -0700

    Back to development: 5.0.0b2

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ long_description = "\n\n".join(
 
 setup(
     name="plone.volto",
-    version="5.0.0b1",
+    version="5.0.0b2.dev0",
     description="Volto integration add-on for Plone",
     long_description=long_description,
     long_description_content_type="text/markdown",

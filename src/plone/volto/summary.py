4fd11963f7a8ea961e837a14646c699caed6629b
David Glick <david@glicksoftware.com>
2025-02-07 13:59:34 -0800

    Add more summary fields for rendering listings

--- src/plone/volto/summary.py
+++ src/plone/volto/summary.py
@@ -5,9 +5,17 @@ from zope.interface import implementer
 @implementer(IJSONSummarySerializerMetadata)
 class JSONSummarySerializerMetadata:
     def default_metadata_fields(self):
+        # Fields to include in the default catalog-based summary
+        # serialization in plone.restapi. We try to include everything
+        # needed to render useful listings/teasers.
         return {
             "head_title",
+            "effective",
+            "end",
+            "getObjSize",
             "image_field",
             "image_scales",
+            "mime_type",
             "nav_title",
+            "start",
         }

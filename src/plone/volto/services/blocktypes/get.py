78d066ec497323b4530c0a7b758bfba7349147af
Jonas Piterek <piterek@kitconcept.com>
2025-05-31 16:07:14 +0200

    feat: aggregate block type counts when no specific block type is provided

--- src/plone/volto/services/blocktypes/get.py
+++ src/plone/volto/services/blocktypes/get.py
@@ -4,6 +4,8 @@ from plone.restapi.services import Service
 from zope.interface import implementer
 from zope.publisher.interfaces import IPublishTraverse
 
+import collections
+
 
 @implementer(IPublishTraverse)
 class BlockTypesGet(Service):
@@ -18,27 +20,38 @@ class BlockTypesGet(Service):
     def reply(self):
         catalog = api.portal.get_tool(name="portal_catalog")
         request_body = self.request.form
-        result = {"items": []}
+        result = {}
         type = self.block_type
 
         query = {
             "object_provides": IBlocks.__identifier__,
-            "block_types": self.block_type,
         }
 
         if request_body.get("path"):
             query["path"] = request_body["path"]
 
         if type:
+            query["block_types"] = self.block_type
+            result.setdefault(self.block_type, [])
             brains = catalog.unrestrictedSearchResults(**query)
 
             for brain in brains:
-                result["items"].append(
+                result[self.block_type].append(
                     {
                         "@id": brain.getURL(),
                         "title": brain.Title,
                         "count": brain.block_types[self.block_type],
                     }
                 )
+        else:
+            brains = catalog.unrestrictedSearchResults(**query)
+            block_types_total = collections.Counter()
+
+            for brain in brains:
+                block_types_total.update(brain.block_types)
+
+            for block_type, count in block_types_total.items():
+                result.setdefault(block_type, None)
+                result[block_type] = count
 
         return result

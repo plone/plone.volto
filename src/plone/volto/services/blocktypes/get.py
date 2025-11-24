e6deb930483572eb719885d2901a06a676c3458a
jnptk <110389276+jnptk@users.noreply.github.com>
2025-11-24 14:17:09 +0100

    return array if type is provided

--- src/plone/volto/services/blocktypes/get.py
+++ src/plone/volto/services/blocktypes/get.py
@@ -20,7 +20,6 @@ class BlockTypesGet(Service):
     def reply(self):
         catalog = api.portal.get_tool(name="portal_catalog")
         request_body = self.request.form
-        result = {}
         type = self.block_type
 
         query = {
@@ -31,12 +30,12 @@ class BlockTypesGet(Service):
             query["path"] = request_body["path"]
 
         if type:
-            result.setdefault("items", [])
+            result = []
             query["block_types"] = self.block_type
             brains = catalog.unrestrictedSearchResults(**query)
 
             for brain in brains:
-                result["items"].append(
+                result.append(
                     {
                         "@id": brain.getURL(),
                         "title": brain.Title,
@@ -44,7 +43,7 @@ class BlockTypesGet(Service):
                     }
                 )
         else:
-            result.setdefault("summary", {})
+            result = {}
             brains = catalog.unrestrictedSearchResults(**query)
             block_types_total = collections.Counter()
 
@@ -52,6 +51,6 @@ class BlockTypesGet(Service):
                 block_types_total.update(brain.block_types)
 
             for block_type, count in block_types_total.items():
-                result["summary"][block_type] = count
+                result[block_type] = count
 
         return result
